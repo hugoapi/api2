f585855285af9c0cafbed28fa38442c1d217c885
hugoapi <hugobaugnies2000@gmail.com>
2024-06-28 19:28:28 +0200

    Delete chargement.py

--- chargement.py
+++
@@ -1,8 +0,0 @@
-import pickle
-
-# Charger le modèle avec pickle
-with open("model.pkl", 'rb') as f:
-    model = pickle.load(f)
-
-# Sauvegarder le modèle avec joblib
-joblib.dump(model, "model_new.pkl")
